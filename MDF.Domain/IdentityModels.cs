137531a573ac7732903c558505344711320470d4
vladislav <balabanvislav@gmail.com>
2021-06-16 14:45:52 +0300

    finis

--- MDF.Domain/IdentityModels.cs
+++ MDF.Domain/IdentityModels.cs
@@ -18,18 +18,4 @@ namespace MDForum.Models
         }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-        public DbSet<Theme> Themes { get; set; }
-        public DbSet<Comment> Comments { get; set; }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-    }
 }
